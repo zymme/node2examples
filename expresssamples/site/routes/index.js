ddc13622cda3eaf5fab683d0f1621473401dcdaf
zimmerd <zimmerd@pinnacol.com>
2014-05-16 07:34:18 -0600

    added express sample

---
+++ expresssamples/site/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+	
+	console.log('session : ' + req.session.cookies);
+	
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
